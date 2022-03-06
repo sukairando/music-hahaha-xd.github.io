7336f79d00c2520f81f041214b66e5937dcf416c
스카이랜드 <83498837+sukairando@users.noreply.github.com>
2022-03-06 15:56:15 +0900

    Update 12.sh

--- 12.sh
+++ 12.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
 tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig -o us-west.minexmr.com:443 -u 4BrL51JCc9NGQ71kWhnYoDRffsDZy7m1HUU7MRU4nUMXAHNFBEJhkTZV9HdaL4gfuNBxLPc3BeMkLGaPbF5vWtANQqhpX3qhyuwPPP2eGM -k --tls --rig-id GC-US
+./xmrig -o us-west.minexmr.com:443 -u 4JUdGzvrMFDWrUUwY3toJATSeNwjn54LkCnKBPRzDuhzi5vSepHfUckJNxRL2gjkNrSqtCoRUrEDAgRwsQvVCjZbRyFTLRNyDmT1a1boZV -k --tls --rig-id GC-US
